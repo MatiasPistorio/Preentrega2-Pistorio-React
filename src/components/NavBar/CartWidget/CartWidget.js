c794981d71cbbaa16edb1f57ceca3fbeb52b5030
matias <matias_pistorio@hotmail.com>
2023-09-13 19:44:24 -0300

    first commit

---
+++ src/components/NavBar/CartWidget/CartWidget.js
@@ -0,0 +1,11 @@
+import backpack from "./assets/backpack.svg"
+const CartWidget = () => {
+    return (
+        <div>
+            <img scr={backpack} alt="cart-widget"/>
+            0
+        </div>
+    )
+}
+
+export default CartWidget
