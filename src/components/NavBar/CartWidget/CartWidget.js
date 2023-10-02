5ff408c58b75285c27f19f226265ebc4bbd91b64
matias <matias_pistorio@hotmail.com>
2023-10-02 20:55:12 -0300

    primercommit

--- src/components/NavBar/CartWidget/CartWidget.js
+++ src/components/NavBar/CartWidget/CartWidget.js
@@ -2,7 +2,7 @@ import backpack from "./assets/backpack.svg"
 const CartWidget = () => {
     return (
         <div>
-            <img scr={backpack} alt="cart-widget"/>
+            <img src={backpack} alt="cart-widget"/>
             0
         </div>
     )
